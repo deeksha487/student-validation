648c0a7a5671e0e83a7300f6364135aa37face9a
deeksha487 <deekshavernekar11@gmail.com>
2025-11-19 11:28:01 +0530

    Update student_validate.py

--- student_validate.py
+++ student_validate.py
@@ -1,30 +1,31 @@
 import sys
+if len(sys.argv)!=5:
+   print("usage:student_validate.py <name> <reg_no> <email> <dept>")
+   sys.exit()
 
-if len(sys.argv) != 5:
-    print("usage: student_validate.py <name> <reg_no> <email> <dept>")
-    sys.exit()
+name=sys.argv[1]
+reg_no=sys.argv[2]
+email=sys.argv[3]
+dept=sys.argv[4]
 
-    name = sys.argv[1]
-    reg_no = sys.argv[2]
-    email = sys.argv[3]
-    dept = sys.argv[4]
-    
-valid=true
-if not reg_no is digit():
-    print("invalid registration")
-    
-if @ not in email():
-    print("please enter the correct email id")
+valid = True
 
+if not reg_no.isdigit():
+    print("Invalid registration number ,please enter the correct registration number:")
+    valid = False
+
+if "@" not in email:
+    print("Invalid email ,please enter valid email id")
+    valid = False
 
 if valid:
-    print("Registration successful!")
-    print("student name:",name)
-    print("Student Registration number:", reg_no)
-    print("Email:", email)
-    print("Department:", dept)
+    print("Registration successful")
+    print("Student Name:",name)
+    print("Student Register Number",reg_no)
+    print("Email:",email)
+    print("Department:",dept)
 else:
-    print("name:", "deeksha")
+    print("name:",deeksha)  
+    print("reg_no:",62)  
     print("email:",deeksha@11)
-    print("reg_no:", 62)
-    print("dept:", "bca")
+    print("dept:",bca)
