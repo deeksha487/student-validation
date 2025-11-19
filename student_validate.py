92c9ef7849760f60c96679247a15a1a433452906
deeksha <deekshavernekar11@gmail.com>
2025-11-19 10:52:25 +0530

    save

--- student_validate.py
+++ student_validate.py
@@ -4,10 +4,11 @@ if len(sys.argv) != 5:
     print("usage: student_validate.py <name> <reg_no> <email> <dept>")
     sys.exit()
 
-name = sys.argv[1]
-reg_no = sys.argv[2]
-email = sys.argv[3]
-dept = sys.argv[4]
+    name = sys.argv[1]
+    reg_no = sys.argv[2]
+    email = sys.argv[3]
+    dept = sys.argv[4]
+
 
 valid = True
 
@@ -17,10 +18,12 @@ if not reg_no.isdigit():
 
 if valid:
     print("Registration successful!")
+    print("student name:",name)
     print("Student Registration number:", reg_no)
     print("Email:", email)
     print("Department:", dept)
 else:
     print("name:", "deeksha")
+    print("email:",deeksha@11)
     print("reg_no:", 62)
     print("dept:", "bca")
